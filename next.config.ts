004bcbac4d5701de66e64a063558a1434198035a
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-06-10 07:46:48 +0900

    impl: output /docs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+  distDir: "docs",
+  assetPrefix: isProduction ? "/reservation-site/" : "",
+  trailingSlash: true,
 };
 
 export default nextConfig;
